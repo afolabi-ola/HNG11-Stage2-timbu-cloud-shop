a6bc362e508f5d48cd432051e3135779522ba9d4
afolabi-ola <afolabiquadri28@gmail.com>
2024-07-07 18:43:11 +0100

    completed-products-mapping

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,20 @@
 import './Button.css';
 
-const Button = ({ children, bgColor = '#000000', color = '#ffffff' }) => {
+const Button = ({
+  children,
+  bgColor = '#000000',
+  color = '#ffffff',
+  width = '5em',
+}) => {
   const styles = {
     backgroundColor: bgColor,
     color: color,
+    width: width,
   };
 
   return (
     <div className='button'>
-      <button style={styles}>{children}</button>{' '}
+      <button style={styles}>{children}</button>
     </div>
   );
 };
