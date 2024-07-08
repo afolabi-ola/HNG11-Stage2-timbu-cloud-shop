9ed507ed2352131dd55e4d8c957a6d9c76e2f8aa
afolabi-ola <afolabiquadri28@gmail.com>
2024-07-08 18:40:19 +0100

    first page responsive

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,30 @@
 import './Logo.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
+  faBars,
   faShoppingCart,
+  faUser,
 } from '@fortawesome/free-solid-svg-icons';
 
 const Logo = () => {
   return (
-    <div className='logo'>
-      <p>Flexmart</p>
-      <p className='logo-icon'>
+    <div className='logo-overall'>
+      <div className='icon1'>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+
+      <div className='logo'>
+        <p>Flexmart</p>
+        <p className='logo-icon'>
+          <FontAwesomeIcon icon={faShoppingCart} />
+        </p>
+      </div>
+      <div className='icon2'>
+        <FontAwesomeIcon icon={faUser} />
+      </div>
+      <div className='icon3'>
         <FontAwesomeIcon icon={faShoppingCart} />
-      </p>
+      </div>
     </div>
   );
 };
